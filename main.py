551b31d525f9ca947956839844adad72b582bae3
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-25 20:43:51 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,113 @@
+Here's an improved version of the Python program:
+
+```python
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import webbrowser
+import smtplib
+
+class Assistant:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+
+    def listen(self):
+        with self.microphone as source:
+            print("Listening...")
+            self.recognizer.pause_threshold = 1
+            audio = self.recognizer.listen(source)
+
+        try:
+            print("Recognizing...")
+            query = self.recognizer.recognize_google(audio, language='en')
+            print(f"User said: {query}\n")
+            return query.lower()
+        except sr.UnknownValueError:
+            print("I'm sorry, I didn't catch that. Can you please repeat?")
+        except sr.RequestError:
+            print("Sorry, I am unable to access the speech recognition service.")
+        return None
+
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+
+    def greet_user(self):
+        hour = int(datetime.datetime.now().hour)
+
+        if hour < 12:
+            self.speak("Good morning!")
+        elif 12 <= hour < 18:
+            self.speak("Good afternoon!")
+        else:
+            self.speak("Good evening!")
+
+        self.speak("How can I assist you today?")
+
+    def send_email(self, to, subject, body, sender_email, sender_password):
+        try:
+            with smtplib.SMTP('smtp.gmail.com', 587) as server:
+                server.ehlo()
+                server.starttls()
+                server.login(sender_email, sender_password)
+                message = f"Subject: {subject}\n\n{body}"
+                server.sendmail(sender_email, to, message)
+            self.speak("Email has been sent successfully!")
+        except Exception as e:
+            print(str(e))
+            self.speak("Sorry, I am unable to send the email at the moment.")
+
+    def process_command(self, command, sender_email, sender_password):
+        if "open" in command:
+            if "browser" in command:
+                webbrowser.open("https://www.google.com")
+            elif "calculator" in command:
+                os.startfile("calc.exe")
+            elif "notepad" in command:
+                os.startfile("notepad.exe")
+            else:
+                self.speak("Sorry, I don't know how to open that application.")
+        elif "tell me the time" in command:
+            current_time = datetime.datetime.now().strftime("%H:%M")
+            self.speak(f"The current time is {current_time}")
+        elif "send email" in command:
+            self.speak("To whom should I send the email?")
+            recipient = self.listen()
+            self.speak("What should be the subject?")
+            subject = self.listen()
+            self.speak("What should be the body of the email?")
+            body = self.listen()
+            self.send_email(recipient, subject, body, sender_email, sender_password)
+        elif "quit" in command:
+            self.speak("Goodbye!")
+            exit()
+        else:
+            self.speak("Sorry, I don't understand that command.")
+
+    def run(self, sender_email, sender_password):
+        self.greet_user()
+        while True:
+            command = self.listen()
+            if command:
+                self.process_command(command, sender_email, sender_password)
+
+if __name__ == "__main__":
+    assistant = Assistant()
+    assistant.run("your_email@example.com", "your_email_password")
+```
+
+Here are the improvements made:
+1. Converted the program into a class-based structure to encapsulate functionality and improve code organization.
+2. Improved error handling in the `listen()` function to handle various speech recognition errors and provide appropriate error messages.
+3. Used a `with` statement when sending emails with `smtplib` to ensure proper cleanup of the SMTP connection.
+4. Encouraged passing email sender credentials as arguments to the `run()` method instead of hardcoding them in the program for better security and flexibility.
+   
+Additionally, you may consider further improvements such as:
+1. Adding exception handling for potential errors during web browser opening and file execution.
+2. Implementing additional features like playing music, setting reminders, or fetching information from APIs.
+3. Creating separate functions or methods for each command in the `process_command()` method to improve readability and maintainability.
+4. Implementing error handling for the email sending process, such as validating email addresses and checking for network connectivity.
+
+Remember to periodically update the email sending mechanism to use secure authentication methods if applicable.
