abff19be484dc74d44a6592bbfec676652d44d4e
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:08:38 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,39 +1,256 @@
-* Commit 1: Refactor the script to use a class -based structure for better organization and encapsulation of functionality.
-- Create a new file called `assistant.py`.
-- Move the existing code into a class called `Assistant`.
-- Add an `__init__` method to initialize the `recognizer` and `microphone` objects.
-- Move the `listen`, `speak`, `greet_user`, `send_email`, `process_command`, and `run` methods into the class .
-- Remove the `import ` statements for `smtplib`, `webbrowser`, `datetime`, `pyttsx3`, and `speech_recognition`.
-- Update the `if __name__ == "__main__"` block to instantiate an instance of the `Assistant` class and call the `run` method.
+* Commit 1:
 
-* Commit 2: Improve error handling in the `listen` method.
-- Wrap the code inside the `with self.microphone as source` block in a try -except block.
-- Catch the `sr.UnknownValueError` exception and print a helpful error message.
-- Catch the `sr.RequestError` exception and print a helpful error message.
+```python
+# Create a new file called `assistant.py`.
 
-* Commit 3: Use a `with ` statement when sending emails with `smtplib` for proper cleanup.
-- Wrap the `smtplib.SMTP` connection in a `with ` statement.
-    - Move the existing code that logs in , sends the email, and closes the connection inside the `with` block.
 
-* Commit 4: Encourage passing email sender credentials as arguments to the `run` method instead of hardcoding them.
-- Remove the hardcoded email and password from the `run` method arguments.
-- Update the `run` method to accept `sender_email` and `sender_password` as arguments.
-- Replace the hardcoded email and password in the `run` method call in the `__main__` block with actual email and password values.
+class Assistant:
+    def __init__(self):
+        self.recognizer = speech_recognition.Recognizer()
+        self.microphone = speech_recognition.Microphone()
 
-* Commit 5: Add exception handling for web browser opening and file execution.
-- Wrap the `webbrowser.open` and `os.startfile` calls in try -except blocks.
-- Catch any exceptions that may occur and print a helpful error message.
+    def listen(self):
+        try:
+            with self.microphone as source:
+                self.recognizer.adjust_for_ambient_noise(source)
+                audio = self.recognizer.listen(source)
+            return self.recognizer.recognize_google(audio)
+        except speech_recognition.UnknownValueError:
+            print("Sorry, I didn't catch that. Can you please repeat?")
+            return None
+        except speech_recognition.RequestError:
+            print(
+                "Sorry, I'm having trouble with my speech recognition service. Please try again later.")
+            return None
 
-* Commit 6: Implement additional features like playing music, setting reminders, or fetching information from APIs.
-- Add new methods to the `Assistant` class for each additional feature.
-- Implement the functionality for each feature in its respective method.
+    def speak(self, text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
 
-* Commit 7: Create separate functions or methods for each command in the `process_command` method.
-- Refactor the `process_command` method by creating separate methods for each command.
-- Update the `process_command` method to call the appropriate method based on the command.
+    def greet_user(self):
+        current_hour = datetime.datetime.now().hour
+        if current_hour < 12:
+            self.speak("Good morning!")
+        elif current_hour < 18:
+            self.speak("Good afternoon!")
+        else:
+            self.speak("Good evening!")
 
-* Commit 8: Implement error handling for the email sending process.
-- Add additional error handling code to validate email addresses, check network connectivity, and handle other potential errors.
-- Print helpful error messages for each specific error scenario.
+    def send_email(self, sender_email, sender_password, recipient_email, subject, message):
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipient_email,
+                            f"Subject: {subject}\n\n{message}")
 
-Remember to periodically update the email sending mechanism to use secure authentication methods if applicable.
+    def process_command(self, command):
+        if "greet" in command:
+            self.greet_user()
+        elif "send email" in command:
+            sender_email = "your_email@gmail.com"
+            sender_password = "your_password"
+            recipient_email = "recipient_email@gmail.com"
+            subject = "Test Email"
+            message = "This is a test email."
+            self.send_email(sender_email, sender_password,
+                            recipient_email, subject, message)
+
+    def run(self, sender_email, sender_password):
+        self.greet_user()
+        while True:
+            command = self.listen()
+            if command:
+                self.process_command(command)
+
+
+if __name__ == "__main__":
+    assistant = Assistant()
+    assistant.run("your_email@gmail.com", "your_password")
+```
+
+* Commit 2:
+
+```python
+   def listen(self):
+        try:
+            with self.microphone as source:
+                self.recognizer.adjust_for_ambient_noise(source)
+                audio = self.recognizer.listen(source)
+            return self.recognizer.recognize_google(audio)
+        except speech_recognition.UnknownValueError:
+            print("Sorry, I didn't catch that. Can you please repeat?")
+            return None
+        except speech_recognition.RequestError:
+            print(
+                "Sorry, I'm having trouble with my speech recognition service. Please try again later.")
+            return None
+```
+
+* Commit 3:
+
+```python
+   def send_email(self, sender_email, sender_password, recipient_email, subject, message):
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipient_email,
+                            f"Subject: {subject}\n\n{message}")
+```
+
+* Commit 4:
+
+```python
+   def run(self, sender_email, sender_password):
+        self.greet_user()
+        while True:
+            command = self.listen()
+            if command:
+                self.process_command(command)
+
+if __name__ == "__main__":
+    assistant = Assistant()
+    assistant.run("your_email@gmail.com", "your_password")
+```
+
+* Commit 5:
+
+```python
+
+
+def open_web_browser(url):
+    try:
+        webbrowser.open(url)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def execute_file(file_path):
+    try:
+        os.startfile(file_path)
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Update the process_command method:
+
+    def process_command(self, command):
+        if "greet" in command:
+            self.greet_user()
+        elif "send email" in command:
+            # ...
+        elif "open browser" in command:
+            url = "https://www.google.com"
+            open_web_browser(url)
+        elif "execute file" in command:
+            file_path = "path/to/file.exe"
+            execute_file(file_path)
+
+
+```
+
+* Commit 6:
+
+```python
+   def play_music(self):
+        # Implement music playing functionality here
+
+    def set_reminder(self):
+        # Implement reminder setting functionality here
+
+    def fetch_information(self):
+        # Implement information fetching functionality here
+
+    # Update the process_command method:
+
+    def process_command(self, command):
+        if "greet" in command:
+            self.greet_user()
+        elif "send email" in command:
+            # ...
+        elif "open browser" in command:
+            # ...
+        elif "execute file" in command:
+            # ...
+        elif "play music" in command:
+            self.play_music()
+        elif "set reminder" in command:
+            self.set_reminder()
+        elif "fetch information" in command:
+            self.fetch_information()
+```
+
+* Commit 7:
+
+```python
+   def process_greet_command(self):
+        self.greet_user()
+
+    def process_email_command(self):
+        # Implement email sending functionality here
+
+    def process_browser_command(self):
+        # Implement web browser opening functionality here
+
+    def process_file_command(self):
+        # Implement file execution functionality here
+
+    def process_play_music_command(self):
+        self.play_music()
+
+    def process_set_reminder_command(self):
+        self.set_reminder()
+
+    def process_fetch_information_command(self):
+        self.fetch_information()
+
+    def process_command(self, command):
+        if "greet" in command:
+            self.process_greet_command()
+        elif "send email" in command:
+            self.process_email_command()
+        elif "open browser" in command:
+            self.process_browser_command()
+        elif "execute file" in command:
+            self.process_file_command()
+        elif "play music" in command:
+            self.process_play_music_command()
+        elif "set reminder" in command:
+            self.process_set_reminder_command()
+        elif "fetch information" in command:
+            self.process_fetch_information_command()
+```
+
+* Commit 8:
+
+```python
+
+
+def validate_email(email):
+    # Implement email validation logic here
+
+
+def check_network_connectivity():
+    # Implement network connectivity check logic here
+
+    # Update the send_email method:
+
+
+def send_email(self, sender_email, sender_password, recipient_email, subject, message):
+    try:
+        validate_email(sender_email)
+        validate_email(recipient_email)
+        check_network_connectivity()
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, recipient_email,
+                            f"Subject: {subject}\n\n{message}")
+
+    except InvalidEmailError:
+        print("Invalid email address!")
+    except NetworkError:
+        print("No network connectivity!")
+    except Exception as e:
+        print(f"Error: {e}")
+```
